3cb4454936d2f4de203f03bb8f20f25782d648d2
Scene-ry <cy3wjx@126.com>
2016-05-15 16:24:32 +0800

    JDBC Test

---
+++ src/main/java/com/example/controller/MovieController.java
@@ -0,0 +1,43 @@
+package com.example.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.example.model.Movie;
+import com.example.service.*;
+
+@Controller
+@RequestMapping("/movie")
+public class MovieController {
+
+	@Autowired
+	private MovieService movieService;
+	
+	@RequestMapping
+	public String index(ModelMap modelMap) {
+		modelMap.addAttribute(new Movie());
+		modelMap.put("movies", movieService.findAll());
+		return "/movie/index";
+	}
+	
+	@RequestMapping(value = "/detail/{id}")
+	public String detail(@PathVariable("id") int id, ModelMap modelMap) {
+		modelMap.put("movie", movieService.find(id));
+		return "/movie/detail";
+	}
+	
+	@RequestMapping(method = RequestMethod.POST)
+	public String addMovie(Movie movie, final BindingResult bindingResult, final ModelMap model) {
+		if (bindingResult.hasErrors()) {
+            return "/movie/index";
+        }
+        movieService.addMovie(movie);
+        model.clear();
+        return "redirect:/movie";
+	}
+}
