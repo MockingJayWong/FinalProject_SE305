b077fbb3ee0b93baa42c464a054a6eace053abba
Scene-ry <cy3wjx@126.com>
2016-05-16 21:11:19 +0800

    aaa

--- src/main/java/com/example/dao/SeatListDAOImpl.java
+++ src/main/java/com/example/dao/SeatListDAOImpl.java
@@ -1,5 +1,78 @@
 package com.example.dao;
 
-public class SeatListDAOImpl {
+import java.sql.Date;
+import java.util.Collection;
+import java.util.ArrayList;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+import com.example.model.*;
+
+public class SeatListDAOImpl implements SeatListDAO {
+
+	@Autowired
+	private JdbcTemplate jdbcTemplate;
+	
+	@Override
+	public Collection<Seat> findAll() {
+		return jdbcTemplate.query("select * from seat",
+				new BeanPropertyRowMapper<Seat>(Seat.class));
+	}
+
+	// Date comparing dai ding
+	@Override
+	public Collection<Seat> findByCinemaIdAndHall(int cinemaID, int hall, Date time) {
+		Collection<Seat> result = new ArrayList<Seat>();
+		Collection<Session> sessions = jdbcTemplate.query("select * from session where cinemaID = ? and hall = ? and time >= start_time and time <= end_time",
+				new BeanPropertyRowMapper<Session>(Session.class),
+				new Object[] { cinemaID, hall, time });
+		
+		for (Session session : sessions) {
+			result.addAll(jdbcTemplate.query("select * from seat where sessionID = ?",
+					new BeanPropertyRowMapper<Seat>(Seat.class), session.getId()));
+		}
+		return result;
+	}
+
+	@Override
+	public Seat findBySessionId(int sessionID) {
+		try {
+			return jdbcTemplate.queryForObject("select * from seat where sessionID = ?",
+					new BeanPropertyRowMapper<Seat>(Seat.class), sessionID);
+		}
+		catch (IndexOutOfBoundsException e) {
+			return null;
+		}
+	}
+
+	@Override
+	public Seat findById(int seatID) {
+		try {
+			return jdbcTemplate.query("select * from seat where id = ?",
+					new BeanPropertyRowMapper<Seat>(Seat.class), seatID).get(0);
+		}
+		catch (IndexOutOfBoundsException e) {
+			return null;
+		}
+	}
+
+	@Override
+	public int insert(Seat seat) {
+		return jdbcTemplate.update("insert into seat(sessionID, sold_list) values(?, ?)",
+				new Object[] { seat.getSessionID(), seat.getSold_list() });
+	}
+
+	@Override
+	public int update(Seat seat) {
+		return jdbcTemplate.update("update ticket set sessionID = ?, sold_list = ? where id = ?",
+				new Object[] { seat.getSessionID(), seat.getSold_list(), seat.getId() });
+	}
+
+	@Override
+	public int delete(int seatID) {
+		return jdbcTemplate.update("delete from seat where id = ?", seatID);
+	}
 
 }
--- src/main/java/com/example/dao/TicketImpl.java
+++ src/main/java/com/example/dao/TicketImpl.java
@@ -2,44 +2,50 @@ package com.example.dao;
 
 import java.util.Collection;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
+import org.springframework.jdbc.core.JdbcTemplate;
+
 import com.example.model.Ticket;
 
 public class TicketImpl implements TicketDAO{
+	
+	@Autowired
+	private JdbcTemplate jdbcTemplate;
 
 	@Override
 	public Collection<Ticket> findAll() {
-		// TODO Auto-generated method stub
-		return null;
+		return jdbcTemplate.query("select * from ticket",
+				new BeanPropertyRowMapper<Ticket>(Ticket.class));
 	}
 
 	@Override
 	public Collection<Ticket> findByOrder(int orderID) {
-		// TODO Auto-generated method stub
-		return null;
+		return jdbcTemplate.query("select * from ticket where orderID = ?",
+				new BeanPropertyRowMapper<Ticket>(Ticket.class), orderID);
 	}
 
 	@Override
 	public Collection<Ticket> findBySession(int sessionID) {
-		// TODO Auto-generated method stub
-		return null;
+		return jdbcTemplate.query("select * from ticket where sessionID = ?",
+				new BeanPropertyRowMapper<Ticket>(Ticket.class), sessionID);
 	}
 
 	@Override
 	public int insert(Ticket ticket) {
-		// TODO Auto-generated method stub
-		return 0;
+		return jdbcTemplate.update("insert into ticket(orderID, seat, price, sessionID) values(?, ?, ?, ?)",
+				new Object[] { ticket.getOrderID(), ticket.getSeat(), ticket.getPrice(), ticket.getSessionID() });
 	}
 
 	@Override
 	public int update(Ticket ticket) {
-		// TODO Auto-generated method stub
-		return 0;
+		return jdbcTemplate.update("update ticket set orderID = ?, seat = ?, price = ?, sessionID = ? where id = ?",
+				new Object[] { ticket.getOrderID(), ticket.getSeat(), ticket.getPrice(), ticket.getSessionID(), ticket.getId() });
 	}
 
 	@Override
 	public int delete(int ticketID) {
-		// TODO Auto-generated method stub
-		return 0;
+		return jdbcTemplate.update("delete from ticket where id = ?", ticketID);
 	}
 
 }
