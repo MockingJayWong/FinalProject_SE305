7716544f6af9e59ac8ad83785201f0e066222cc2
Scene-ry <cy3wjx@126.com>
2016-05-24 00:35:16 +0800

    增加一个JUnit示例，可参考之测试DAO和Service

---
+++ src/test/java/com/march/ticketjdbc/test/MovieDAOTest.java
@@ -0,0 +1,24 @@
+package com.march.ticketjdbc.test;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import com.march.ticketjdbc.dao.MovieDAOImpl;
+import com.march.ticketjdbc.model.Movie;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = "file:src/main/webapp/WEB-INF/spring/appServlet/servlet-context.xml")
+public class MovieDAOTest {
+
+	@Autowired
+	private MovieDAOImpl movieDAO;
+	
+	@Test
+	public void addMovie() {
+		Movie movie = new Movie("JUNIT", "a.org", "get pps!", 12.9f);
+		movieDAO.insert(movie);
+	}
+}
