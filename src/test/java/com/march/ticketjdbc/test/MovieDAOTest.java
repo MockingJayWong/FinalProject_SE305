24935f401decd39d66456f45198536119992033b
Scene-ry <cy3wjx@126.com>
2016-05-24 00:58:31 +0800

    增加一个JUnit示例，可参考之测试DAO和Services

--- src/test/java/com/march/ticketjdbc/test/MovieDAOTest.java
+++ src/test/java/com/march/ticketjdbc/test/MovieDAOTest.java
@@ -18,7 +18,7 @@ public class MovieDAOTest {
 	
 	@Test
 	public void addMovie() {
-		Movie movie = new Movie("JUNIT", "a.org", "get pps!", 12.9f);
+		Movie movie = new Movie("JUNIT", "a.org", "my god!", 12.9f);
 		movieDAO.insert(movie);
 	}
 }
