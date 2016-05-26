6fe38db8fab0b2b300d8c1658ee88d9c182cbb3b
wenboy1314 <wenboy13142@qq.com>
2016-05-26 22:12:53 +0800

    Update cinema.sql
    update movie model and change date to long

--- cinema.sql
+++ cinema.sql
@@ -1,36 +1,37 @@
-CREATE SCHEMA IF NOT EXISTS `cinema_datebase` ;
+CREATE SCHEMA IF NOT EXISTS `cinema_database` ;
 
-drop table if exists`cinema_datebase`.`movie`;
-drop table if exists`cinema_datebase`.`user`;
-drop table if exists`cinema_datebase`.`cinema`;
-drop table if exists`cinema_datebase`.`orders`;
-drop table if exists`cinema_datebase`.`session`;
-drop table if exists`cinema_datebase`.`ticket`;
-drop table if exists`cinema_datebase`.`seat`;
+drop table if exists`cinema_database`.`movie`;
+drop table if exists`cinema_database`.`user`;
+drop table if exists`cinema_database`.`cinema`;
+drop table if exists`cinema_database`.`orders`;
+drop table if exists`cinema_database`.`session`;
+drop table if exists`cinema_database`.`ticket`;
+drop table if exists`cinema_database`.`seat`;
 
-CREATE TABLE `cinema_datebase`.`movie` (
+CREATE TABLE `cinema_database`.`movie` (
   `id` INT NOT NULL AUTO_INCREMENT ,
   `movieName` VARCHAR(45) NOT NULL ,
   `url` TEXT DEFAULT NULL ,
   `introduction` TEXT DEFAULT NULL ,
   `score` FLOAT NOT NULL ,
-  `start_time` DATE NOT NULL,
-  `end_time` DATE NOT NULL,
+  `start_time` LONG NOT NULL,
+  `end_time` LONG NOT NULL,
+  `type` TEXT,
+  `director` TEXT NOT NULL,
+  `actor` TEXT NOT NULL,
   PRIMARY KEY (`id`)
 )ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-CREATE  TABLE `cinema_datebase`.`user` (
+CREATE  TABLE `cinema_database`.`user` (
   `id` INT NOT NULL AUTO_INCREMENT ,
   `username` VARCHAR(45) NOT NULL ,
   `password` VARCHAR(45) NOT NULL ,
   `telephone` VARCHAR(11) NOT NULL ,
   `email` VARCHAR(45) NOT NULL ,
-   /*0代表普通用户， 1代表影院用户*/
-  `role` enum('0', '1') NOT NULL ,
   PRIMARY KEY (`id`)
  )ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-CREATE TABLE `cinema_datebase`.`cinema` (
+CREATE TABLE `cinema_database`.`cinema` (
   `id` INT NOT NULL AUTO_INCREMENT ,
   `cinemaName` VARCHAR(45) NOT NULL,
   `address` VARCHAR(45) NOT NULL,
@@ -41,45 +42,45 @@ CREATE TABLE `cinema_datebase`.`cinema` (
   PRIMARY KEY(`id`)
 )ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-CREATE TABLE `cinema_datebase`.`orders` (
+CREATE TABLE `cinema_database`.`orders` (
   `id` INT NOT NULL AUTO_INCREMENT ,
   `userID` INT NOT NULL ,
   `cinemaID` INT NOT NULL ,
-  `time` DATE NOT NULL ,
+  `time` LONG NOT NULL ,
   `prices` FLOAT NOT NULL ,
   /*0代表已付款，1代表未付款，2代表已失效*/
   `state` enum('0', '1', '2') NOT NULL, 
   PRIMARY KEY(`id`),
   CONSTRAINT `user_orders_id`
     FOREIGN KEY(`userID`)
-    REFERENCES `cinema_datebase`.`user` (`id`)
+    REFERENCES `cinema_database`.`user` (`id`)
     ON DELETE NO ACTION
     ON UPDATE CASCADE ,
   CONSTRAINT `cinema_orders_id`
     FOREIGN KEY(`cinemaID`)
-    REFERENCES `cinema_datebase`.`cinema` (`id`)
+    REFERENCES `cinema_database`.`cinema` (`id`)
     ON DELETE NO ACTION
     ON UPDATE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-CREATE TABLE `cinema_datebase`.`session` (
+CREATE TABLE `cinema_database`.`session` (
   `id` INT NOT NULL AUTO_INCREMENT ,
   `cinemaID` INT NOT NULL ,
   `hall` INT NOT NULL ,
   `movieName` VARCHAR(45) NOT NULL ,
-  `start_time` DATE NOT NULL ,
-  `end_time` DATE NOT NULL ,
+  `start_time` LONG NOT NULL ,
+  `end_time` LONG NOT NULL ,
   `language` VARCHAR(45) NOT NULL ,
   `price` FLOAT NOT NULL ,
   PRIMARY KEY(`id`) ,
   CONSTRAINT `cinema_session_id`
     FOREIGN KEY (`cinemaID`)
-    REFERENCES `cinema_datebase`.`cinema` (`id`)
+    REFERENCES `cinema_database`.`cinema` (`id`)
     ON DELETE NO ACTION
     ON UPDATE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-CREATE TABLE `cinema_datebase`.`ticket` (
+CREATE TABLE `cinema_database`.`ticket` (
   `id` INT NOT NULL AUTO_INCREMENT ,
   `orderID` INT NOT NULL ,
   `seat` INT NOT NULL ,
@@ -88,24 +89,24 @@ CREATE TABLE `cinema_datebase`.`ticket` (
   PRIMARY KEY(`id`) ,
   CONSTRAINT `order_ticket_id`
     FOREIGN KEY (`orderID`)
-    REFERENCES `cinema_datebase`.`orders` (`id`)
+    REFERENCES `cinema_database`.`orders` (`id`)
     ON DELETE NO ACTION
     ON UPDATE CASCADE ,
   CONSTRAINT `session_ticket_id`
     FOREIGN KEY (`sessionID`)
-    REFERENCES `cinema_datebase`.`session` (`id`)
+    REFERENCES `cinema_database`.`session` (`id`)
     ON DELETE NO ACTION
     ON UPDATE CASCADE
 )ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-CREATE TABLE `cinema_datebase`.`seat` (
+CREATE TABLE `cinema_database`.`seat` (
   `id` INT NOT NULL AUTO_INCREMENT ,
   `sessionID` INT NOT NULL ,
   `sold_list` TEXT DEFAULT NULL ,
   PRIMARY KEY (`id`) ,
   CONSTRAINT `session_seat_id`
     FOREIGN KEY (`sessionID`)
-    REFERENCES `cinema_datebase`.`session` (`id`)
+    REFERENCES `cinema_database`.`session` (`id`)
     ON DELETE NO ACTION
     ON UPDATE CASCADE
 )ENGINE=InnoDB DEFAULT CHARSET=utf8;
